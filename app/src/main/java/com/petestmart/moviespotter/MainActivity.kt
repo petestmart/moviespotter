763c99e4d2d10415fff585b7f18f600e673d97b3
Pete St. Martin <petestmartin@Petes-MBP.hsd1.mn.comcast.net>
2021-10-17 12:56:25 -0500

    initial commit

---
+++ app/src/main/java/com/petestmart/moviespotter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.petestmart.moviespotter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviespotter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
