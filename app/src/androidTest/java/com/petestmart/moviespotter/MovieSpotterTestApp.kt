cbe87b139a6053130722570c3ace294c732f449e
Pete St. Martin <petestmartin@Petes-MacBook-Pro.local>
2021-10-21 14:25:15 -0500

    dependency issues resolved. Test needs debuggin.

--- app/src/androidTest/java/com/petestmart/moviespotter/MovieSpotterTestApp.kt
+++ app/src/androidTest/java/com/petestmart/moviespotter/MovieSpotterTestApp.kt
@@ -20,7 +20,7 @@ open class MovieSpotterTestApp : Application() {
 
     var url = "http://127.0.0.1:8080"
 
-    fun getBaseUrl(): String {
+    private fun getBaseUrl(): String {
         return url
     }
 
