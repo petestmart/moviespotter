bbae99c24d65b2979558db30a4bfd410a07d7db5
Pete St. Martin <petestmartin@Petes-MacBook-Pro.local>
2021-10-26 00:46:10 -0500

    Unit test added and passing

--- app/src/test/java/com/petestmart/moviespotter/MoviesAdapterTest.kt
+++ app/src/test/java/com/petestmart/moviespotter/MoviesAdapterTest.kt
@@ -1,13 +1,20 @@
 package com.petestmart.moviespotter
 
-import org.junit.Assert.*
 
+import com.petestmart.moviespotter.MoviesAdapterTestObject.moviesAdapterTest
+import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class MoviesAdapterTest {
 
     @Test
-    fun getItemCount() {
-
+    fun moviesAdapterTest() {
+        val key = 20
+        val value = "movies"
+        val result = moviesAdapterTest(key, value)
+        Assert.assertEquals(result,true)
     }
 }
