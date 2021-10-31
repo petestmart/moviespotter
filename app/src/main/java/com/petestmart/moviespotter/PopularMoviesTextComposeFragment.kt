049ae0338ea906a925a3063dbccbd99b786f7310
Pete St. Martin <petestmartin@Petes-MacBook-Pro.local>
2021-10-31 16:47:48 -0500

    cleanup of commented out code

--- app/src/main/java/com/petestmart/moviespotter/PopularMoviesTextComposeFragment.kt
+++ app/src/main/java/com/petestmart/moviespotter/PopularMoviesTextComposeFragment.kt
@@ -81,7 +81,7 @@ class PopularMoviesTextComposeFragment : Fragment() {
                                                 if (text == selectedOption) {
                                                     Color.Magenta
                                                 } else {
-                                                    Color.LightGray
+                                                    Color.Gray
                                                 }
                                             )
                                             .padding(
@@ -93,120 +93,12 @@ class PopularMoviesTextComposeFragment : Fragment() {
                             }
                         }
                     }
-//                    @Composable
-//                    fun MaterialButtonToggleGroup() {
-//                        var selected by remember { mutableStateOf("Android") }
-//
-//                        val buttonGroup = listOf("Popular Movies", "Search Movies")
-//
-////                        Card(
-////                            shape = RoundedCornerShape(4.dp),
-////                            backgroundColor = Color.White,
-////                            modifier = Modifier
-////                                .padding(8.dp)
-////                                .fillMaxWidth()
-////                        ) {
-//                        // A pre-defined composable that's capable of rendering a radio group. It honors the
-//                        // Material Design specification.
-//                        val onSelectedChange = { text: String ->
-//                            selected = text
-//                        }
-//                        Row(
-//                            horizontalArrangement = Arrangement.SpaceEvenly
-//                        ) {
-//                            buttonGroup.forEach { text ->
-//                                Row(Modifier
-//                                    .selectable(
-//                                        selected = (text == selected),
-//                                        onClick = { onSelectedChange(text) }
-//                                    )
-//                                    .padding(horizontal = 16.dp)
-//                                ) {
-//                                    Button(
-////                                        enabled = (text == selected),
-//                                        onClick = { onSelectedChange(text) }
-//                                    ) {
-//                                        Column(
-//                                            horizontalAlignment = Alignment.CenterHorizontally
-//                                        ) {
-//                                            Text(
-//                                                text = text,
-//                                                style = MaterialTheme.typography.body1.merge(),
-////                                                color = Color.DarkGray,
-//                                                modifier = Modifier.padding(horizontal = 16.dp)
-//                                            )
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
                     Surface() {
                         MaterialButtonToggleGroup()
                     }
                 }
             }
         }
-
-
-//            Row(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(horizontal = 16.dp),
-//                horizontalArrangement = Arrangement.SpaceEvenly
-//            ) {
-////                val context = LocalContext.current
-////                val isEnabled by remember { mutableStateOf(default) }
-//                var isEnabled by remember { mutableStateOf(false) }
-////                fun isEnabled() {
-////
-////                }
-//                Button(
-//                    onClick = {},
-//                    modifier = Modifier
-//                        .padding(horizontal = 8.dp),
-//                    shape = RoundedCornerShape(16.dp),
-////                    style = MaterialTheme.typography.body2
-//                ) {
-//                    Text(
-//                        text = "Popular Movies",
-//                        modifier = Modifier
-//                            .padding(all = 3.dp)
-//                            .padding(horizontal = 8.dp)
-//                    )
-//                }
-//                Button(
-//                    onClick = {},
-//                    enabled = false,
-//                    modifier = Modifier
-//                        .padding(horizontal = 8.dp),
-//                    shape = RoundedCornerShape(16.dp)
-//                ) {
-//                    Text(
-//                        text = "Movie Search",
-//                        modifier = Modifier
-//                            .padding(all = 4.dp)
-//                            .padding(horizontal = 8.dp)
-//                    )
-//                }
-//            }
-//            Surface(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(horizontal = 16.dp),
-//                elevation = 2.dp,
-//                shape = RoundedCornerShape(16.dp)
-//            ) {
-//                Column(
-//                    horizontalAlignment = Alignment.CenterHorizontally
-//                ) {
-//                    Text(
-//                        text = "Popular Movies",
-//                        fontWeight = FontWeight.Bold,
-//                        modifier = Modifier.padding(all = 4.dp)
-//                    )
-//                }
-//            }
         return view
     }
 }
