5ae6e053f212510880632c2f301c09de70a0a5d6
Pete St. Martin <petestmartin@Petes-MBP.hsd1.mn.comcast.net>
2021-11-05 19:48:38 -0500

    delete commented out unnecessary code

--- app/src/main/java/com/petestmart/moviespotter/PopularMoviesTextComposeFragment.kt
+++ app/src/main/java/com/petestmart/moviespotter/PopularMoviesTextComposeFragment.kt
@@ -6,24 +6,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.petestmart.moviespotter.ui.theme.MovieSpotterTheme
-import com.google.android.material.button.MaterialButtonToggleGroup
 
 class PopularMoviesTextComposeFragment : Fragment() {
 
