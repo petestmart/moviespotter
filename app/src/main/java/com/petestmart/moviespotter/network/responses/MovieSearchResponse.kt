23ad2a7555ce484acc2e15b3e9cf44b577dabfa4
Pete St. Martin <petestmartin@Petes-MBP.hsd1.mn.comcast.net>
2021-11-08 00:24:56 -0600

    Model, Responses, and Repositories added

--- app/src/main/java/com/petestmart/moviespotter/network/responses/MovieSearchResponse.kt
+++ app/src/main/java/com/petestmart/moviespotter/network/responses/MovieSearchResponse.kt
@@ -1,4 +1,13 @@
 package com.petestmart.moviespotter.network.responses
 
-class MovieSearchResponse {
-}
+import com.google.gson.annotations.SerializedName
+import com.petestmart.moviespotter.network.model.MovieDto
+
+data class MovieSearchResponse(
+
+    @SerializedName("count")
+        var count: Int,
+
+    @SerializedName("results")
+        var movies: List<MovieDto>,
+)
--- app/src/main/java/com/petestmart/moviespotter/repository/MovieRepository_Impl.kt
+++ app/src/main/java/com/petestmart/moviespotter/repository/MovieRepository_Impl.kt
@@ -1,4 +1,21 @@
 package com.petestmart.moviespotter.repository
 
-class MovieRepository_Impl {
+import com.petestmart.moviespotter.domain.model.Movie
+import com.petestmart.moviespotter.network.MovieService
+import com.petestmart.moviespotter.network.model.MovieDtoMapper
+
+class MovieRepository_Impl(
+    private val movieService: MovieService,
+    private val mapper: MovieDtoMapper
+): MovieRepository {
+
+    override suspend fun search(token: String, query: String, page: Int):
+            List<Movie> {
+        return mapper.toDomainList(movieService.search(token, query, page).movies)
+    }
+
+    override suspend fun popular(token: String):
+            List<Movie> {
+        return mapper.toDomainList(movieService.popular(token).movies)
+    }
 }
