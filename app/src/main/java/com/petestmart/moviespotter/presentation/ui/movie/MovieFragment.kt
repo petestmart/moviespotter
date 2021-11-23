960c901b311af5269af5ef5c66f1467a7046d4ad
Pete St. Martin <petestmartin@Petes-MBP.hsd1.mn.comcast.net>
2021-11-23 13:41:56 -0600

    Cleanup

--- app/src/main/java/com/petestmart/moviespotter/presentation/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/petestmart/moviespotter/presentation/ui/movie/MovieFragment.kt
@@ -4,21 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.petestmart.moviespotter.presentation.BaseApplication
+import com.petestmart.moviespotter.presentation.components.CircularIndeterminateProgressBar
+import com.petestmart.moviespotter.presentation.components.DefaultSnackbar
+import com.petestmart.moviespotter.presentation.components.MovieView
+import com.petestmart.moviespotter.presentation.components.util.SnackbarController
+import com.petestmart.moviespotter.presentation.theme.AppTheme
 import com.petestmart.moviespotter.presentation.ui.movie.MovieEvent.*
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MovieFragment : Fragment() {
+
+    @Inject
+    lateinit var application: BaseApplication
+
+    private val snackbarController = SnackbarController(lifecycleScope)
 
     private val viewModel: MovieViewModel by viewModels()
 
@@ -41,13 +59,47 @@ class MovieFragment : Fragment() {
 
                 val loading = viewModel.loading.value
                 val movie = viewModel.movie.value
+                val scaffoldState = rememberScaffoldState()
 
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(
-                        text = movie?.let {
-                            "Selected Movie Title: ${movie.title}"
-                        }?: "Loading...",
-                    )
+                AppTheme(darkTheme = application.isDark.value) {
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        snackbarHost = {
+                            scaffoldState.snackbarHostState
+                        }
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ){
+                            if(loading && movie == null){
+                                Text("Loading...")
+                            } else {
+                                movie?.let {
+                                    if(it.id == null){
+                                        snackbarController.showSnackbar(
+                                            scaffoldState = scaffoldState,
+                                            message = "An error has occurred with this movie selection",
+                                            actionLabel = "Ok"
+                                        )
+                                    } else {
+                                        MovieView(movie = it)
+                                    }
+                                }
+                            }
+                            CircularIndeterminateProgressBar(
+                                isDisplayed = loading,
+                            )
+                            DefaultSnackbar(
+                                snackbarHostState = scaffoldState.snackbarHostState,
+                                onDismiss = {
+                                    scaffoldState.snackbarHostState.currentSnackbarData?.dismiss()
+                                },
+                                modifier = Modifier
+                                    .align(Alignment.BottomCenter)
+                            )
+                        }
+                    }
                 }
             }
         }
