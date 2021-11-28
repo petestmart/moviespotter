cd58f97530225f940e43cada63eef073def6a1db
Pete St. Martin <petestmartin@Petes-MBP.hsd1.mn.comcast.net>
2021-11-28 17:11:19 -0600

    Layout files moved to deprecated folder

---
+++ app/src/androidTest/java/com/petestmart/moviespotter/presentation/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.petestmart.moviespotter.presentation
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.petestmart.moviespotter.R
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest{
+
+    @Test
+    fun test_isActivityInView() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun test_isMainNavHostFragmentInView() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.main_nav_host_fragment)).check(matches(isDisplayed()))
+    }
+}
