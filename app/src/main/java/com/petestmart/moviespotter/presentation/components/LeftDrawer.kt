138a151e23b35cba1c30c511093a3e0c42981eeb
Pete St. Martin <47460632+petestmart@users.noreply.github.com>
2023-02-27 22:18:24 -0600

    Version 107.7

--- app/src/main/java/com/petestmart/moviespotter/presentation/components/LeftDrawer.kt
+++ app/src/main/java/com/petestmart/moviespotter/presentation/components/LeftDrawer.kt
@@ -2,24 +2,33 @@ package com.petestmart.moviespotter.presentation.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Absolute.SpaceBetween
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Bottom
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.BottomStart
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.petestmart.moviespotter.BuildConfig
 import com.petestmart.moviespotter.util.DEFAULT_MOVIE_IMAGE
 
 
 @Composable
-fun LeftDrawer() {
+fun LeftDrawer(
+    onToggleTheme: () -> Unit,
+) {
     Box(
         modifier = Modifier
             .fillMaxSize()
